2e016ded57f0a7763d14c629da30939ab8da419d
Adrián Álvarez <adrian.alvarez@scrm.eu>
2023-08-25 14:59:15 +0200

    AKS terraform

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,39 @@
+locals {
+  nodepools = []
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-cluster"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-dns"
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    max_count           = 1
+    min_count           = 1
+    vm_size             = "Standard_B2s"
+    enable_auto_scaling = false
+  }
+  
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "nodepools" {
+  for_each              = local.nodepools
+  name                  = each.value.name
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  enable_auto_scaling   = true
+  min_count             = each.value.min-count
+  max_count             = each.value.max-count
+  node_taints           = each.value.node_taints
+  vm_size               = each.value.vm_size
+}
